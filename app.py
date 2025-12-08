99d67ed16799b9cf09ed5f507dfe291c61bcdc1c
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2025-12-08 16:31:31 +0100

    finished crud operations and routes

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "<h2>HELLOOOOO</h2>"
+    items = db.get_all_items()
+    return render_template("inventory.html", items=items)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        name = request.form["name"]
+        quantity = int(request.form["quantity"])
+        price = float(request.form["price"])
+
+        db.add_items(name, quantity, price)
+
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
+
+@app.route("/delete/<int:item_id>")
+def delete(item_id):
+    db.delete_item(item_id)
+    return redirect(url_for("home"))
+
+
+@app.route("/edit/<int:item_id>", methods=["GET", "POST"])
+def edit(item_id):
+    item = db.get_item_by_id(item_id)
+
+    if request.method == "POST":
+        name = request.form["name"]
+        quantity = int(request.form["quantity"])
+        price = float(request.form["price"])
+        db.update_item(item_id, name, quantity, price)
+        return redirect(url_for("home"))
+    return render_template("edit.html", item = item)
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -1,7 +1,10 @@
 import sqlite3
 
+DB_NAME = "inventory.db"
+
+
 def init_db():
-    conn = sqlite3.connect("inventory.db")
+    conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
     cur.execute(''' 
         CREATE TABLE IF NOT EXISTS inventory(
@@ -23,8 +26,60 @@ def check_data():
     cr.execute('SELECT * FROM inventory')
     print(cr.fetchall())
 
-    
     conn.close()
+
+    # funkcija s kojom uzimam sve iteme
+
+
+def get_all_items():
+    conn = sqlite3.connect("inventory.db")
+    cr = conn.cursor()
+    cr.execute('SELECT * FROM inventory')
+    items = cr.fetchall()
+    conn.close()
+    return items
+    # funkcija za dodavanje elemenata u magacin odnosno listu vodjenja magacina
+
+
+def add_items(name, quantity, price):
+    conn = sqlite3.connect("inventory.db")
+    cr = conn.cursor()
+    cr.execute('INSERT INTO inventory (name, quantity, price) VALUES (?, ?, ?)',
+               (name, quantity, price))
+    conn.commit()
+    conn.close()
+
+
+
+    # funkcija za brisanje elemenata
+def delete_item(item_id):
+    conn = sqlite3.connect("inventory.db")
+    cr = conn.cursor()
+    cr.execute('DELETE FROM inventory WHERE id=?', (item_id,))
+    conn.commit()
+    conn.close()
+
+
+
+    # funkcija za odabir itema, putem ID-A
+def get_item_by_id(item_id):
+    conn = sqlite3.connect("inventory.db")
+    cr = conn.cursor()
+    cr.execute('SELECT * FROM inventory WHERE id=?', (item_id,))
+    item = cr.fetchone()
+    conn.close()
+    return item
+
+
+    # funkcija za update itema
+def update_item(item_id, name, quantity, price):
+    conn = sqlite3.connect("inventory.db")
+    cr = conn.cursor()
+    cr.execute('UPDATE inventory SET name=?, quantity=?, price=? WHERE id=?',
+               (name, quantity, price, item_id))
+    conn.commit()
+    conn.close()
+
 
 if __name__ == "__main__":
     init_db()
